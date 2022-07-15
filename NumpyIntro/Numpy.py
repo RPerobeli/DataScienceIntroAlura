1bf17b1f34208ccb48e5920f7bc9f2e2ccd2b46a
Rodrigo <rodrigo.costa2015@engenharia.ufjf.br>
2022-07-15 15:21:51 -0300

    Aula Numpy

--- NumpyIntro/Numpy.py
+++ NumpyIntro/Numpy.py
@@ -1,3 +1,29 @@
+# %%
 import numpy as np
 
 # main
+# %%
+np.arange(10)
+# %%
+# Criar arrays numpy
+kmList = np.loadtxt('data/carros-km.txt')
+kmList = kmList.astype(int)
+kmList
+# %%
+Anos = np.loadtxt('data/carros-anos.txt')
+Anos
+# %%
+Idade = 2019 - Anos
+Idade
+# %%
+KmMedio = kmList/Idade
+KmMedio
+# %%
+Dados = np.array([kmList, Idade])
+Dados.shape
+# %%
+#indexação booleana
+Dados[Dados > 100]
+# %%
+Dados[1] >= 10
+# %%
